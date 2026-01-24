af28f7a121f66950d4ec4a3b269b612b183ad817
Nextrated <pearliboy1@gmail.com>
2026-01-24 10:29:50 +0100

    fixed comments

--- src/public/config.js
+++ src/public/config.js
@@ -1,4 +1,4 @@
 // public/config.js
-//export const API_BASE_URL = "https://paystackwalletapp.onrender.com";
-export const API_BASE_URL = "http://localhost:80";
+export const API_BASE_URL = "https://paystackwalletapp.onrender.com";
+// export const API_BASE_URL = "http://localhost:80";
 
